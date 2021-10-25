37af070d508cb783d2f6c599ea91b6bbdd9a76e7
Windows <Windows@LAPTOP-JK8PM4TC>
2021-10-25 20:43:15 -0300

    Versão Secundária

--- 212G2-scm/src/main/java/com/fatec/G2scm/controller/HomeController.java
+++ 212G2-scm/src/main/java/com/fatec/G2scm/controller/HomeController.java
@@ -5,19 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-
 public class HomeController {
-	    @GetMapping("/")
-	    public ModelAndView menu() {
-	    	return new ModelAndView ("paginaMenu");
-	    }
-		@GetMapping("/clientes")
-	    public ModelAndView cadastrarCliente() {
-	        return new ModelAndView("cadastrarClientes");
-	    }
-	    @GetMapping("/fornecedores")
-	    public ModelAndView cadastrarFornecedor() {
-	        return new ModelAndView("cadastrarFornecedor");
-	    }
-}
+	@GetMapping("/login")
+	public ModelAndView autenticacao() {
+		return new ModelAndView("paginaLogin");
+	}
 
+	@GetMapping("/")
+	public ModelAndView home() {
+		return new ModelAndView("paginaMenu");
+	}
+}
